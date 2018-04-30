f486d7476f98ee0fb4319fc6d1044884a892436a
Kaz Walker <me@kaz.codes>
2018-04-30 13:34:08 -0700

    Fix production builds. (disable uglify)

--- webpack.config.js
+++ webpack.config.js
@@ -77,29 +77,6 @@ var commonConfig = {
             ['es2015', { modules: false }]
           ]
         }
-      },
-      // Font loaders
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=app/fonts/[name]-[hash].[ext]'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=app/fonts/[name]-[hash].[ext]'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader?name=app/fonts/[name]-[hash].[ext]'
-      },
-      // SVG loader
-      {
-        test: /(.*)\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=app/svgs/[name]-[hash].[ext]'
-      },
-      // Image loader
-      {
-        test: /(.*)\.(jpe?g|png|gif)$/,
-        loader: 'file-loader?name=app/images/[name]-[hash].[ext]'
       }
     ]
   }
@@ -142,6 +119,29 @@ var devConfig = {
             api_url: 'http://localhost:4000'
           }
         }
+      },
+      // Font loaders
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=app/fonts/[name].[ext]'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=app/fonts/[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=app/fonts/[name].[ext]'
+      },
+      // SVG loader
+      {
+        test: /(.*)\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=app/svgs/[name].[ext]'
+      },
+      // Image loader
+      {
+        test: /(.*)\.(jpe?g|png|gif)$/,
+        loader: 'file-loader?name=app/images/[name].[ext]'
       }
     ]
   }
@@ -160,7 +160,8 @@ var staticBuildConfig     = {
     new ExtractTextPlugin({
       filename: 'assets/css/[name]-[hash].css'
     }),
-    new webpack.optimize.UglifyJsPlugin(),
+    // Breaks things.
+    // new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new CompressionPlugin({
       asset: '[path].gz[query]',
@@ -200,6 +201,29 @@ var staticBuildConfig     = {
             api_url: process.env.API_URL || '/',
           }
         }
+      },
+      // Font loaders
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=assets/fonts/[name]-[hash].[ext]'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=assets/fonts/[name]-[hash].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=assets/fonts/[name]-[hash].[ext]'
+      },
+      // SVG loader
+      {
+        test: /(.*)\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml&name=assets/svgs/[name]-[hash].[ext]'
+      },
+      // Image loader
+      {
+        test: /(.*)\.(jpe?g|png|gif)$/,
+        loader: 'file-loader?name=assets/images/[name]-[hash].[ext]'
       }
     ]
   }
